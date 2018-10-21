5c7e4231845a2c86e72cf8c6d49537428fd7af68
hegde5 <vinayhegde2010@gmail.com>
2018-10-21 04:00:37 +0200

    Add unit tests

---
+++ tests/test_services.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+""" Test Services """
+
+import re
+
+from unittest.mock import patch, MagicMock, ANY
+
+
+@patch('boto3.resource')
+def test_create_new_user(boto_mock):
+    """ Test if a user can be created as expected """
+
+    boto_mock = MagicMock()
+    from chalicelib.services import create_new_user
+
+    user_id = 'johndoe@something.com'
+    res = create_new_user(user_id)
+
+    assert res['user'] == user_id
+    assert re.search(
+        r'otpauth://totp/MyApp:johndoe%40something.com\?secret=(.+)'
+        r'&issuer=MyApp',
+        res['qr_code']) is not None
+    assert re.search(
+        r'https://api.qrserver.com/v1/create-qr-code/\?size=150x150&'
+        r'data=otpauth://totp/MyApp:johndoe%40something.com\?secret=(.+)'
+        r'&issuer=MyApp',
+        res['qr_code_url']
+    ) is not None
+
+
+
+@patch('chalicelib.services.DATABASE.get_user')
+@patch('chalicelib.services.TOTP.verify')
+def test_verify_user_code(verify_mock, boto_mock):
+    """ Test if code verification works as expected """
+
+    mock_val = dict(
+        user_id='johndoe@something.com',
+        secret='J6EBMHHXTCCCZ5RC'
+    )
+    boto_mock.return_value = mock_val
+    verify_mock.return_value = True
+
+    from chalicelib.services import verify_user_code
+
+    user_id = 'johndoe@something.com'
+    code = 'code_returned_by_app'
+
+    res = verify_user_code(user_id, code)
+    assert  res == True
+
+    verify_mock.return_value = False
+    res  = verify_user_code(user_id, code)
+    assert res == False
